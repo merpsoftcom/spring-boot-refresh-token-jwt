77d0c1b9e156f5913b8453356e2a8c1dfdf49a20
Premkumar Kanagaraj <premkumar.k@oracle.com>
2025-05-20 07:32:34 +0530

    Added caching libraries

--- src/main/java/com/bezkoder/spring/security/jwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/bezkoder/spring/security/jwt/SpringBootSecurityJwtApplication.java
@@ -2,8 +2,10 @@ package com.bezkoder.spring.security.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootSecurityJwtApplication {
 
 	public static void main(String[] args) {
